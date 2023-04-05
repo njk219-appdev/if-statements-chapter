cabb50808ce228bb72f623c7652c693bfe875c4c
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-05 00:47:19 +0000

    Half way there

--- conditionals_rps.rb
+++ conditionals_rps.rb
@@ -7,3 +7,21 @@
 # If you need a refresher on the rules of Rock, Paper, Scissors: https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
 p "Please choose rock, paper, or scissors:"
+
+input = gets.chomp
+
+#p input
+
+if input == 'rock'
+  p "You played rock!"
+  p "The computer played scissors!"
+  p "You won!"
+elsif input == 'paper'
+  p "You played paper!"
+  p "The computer played scissors!"
+  p "You lost!"
+else
+  p "You played scissors!"
+  p "The computer played scissors!"
+  p "You tied!"
+end
